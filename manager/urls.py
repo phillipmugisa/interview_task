bd6729bd6a5d38e1440941456d37cdebfb329468
phillipmugisa <phillipmugisa4@gmail.com>
2024-02-17 13:50:00 +0300

    task done

--- manager/urls.py
+++ manager/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from manager import views as ManagerViews
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = "manager"
 
 urlpatterns = [
     path("", ManagerViews.HomeView.as_view(), name="home")
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- manager/views.py
+++ manager/views.py
@@ -11,23 +11,29 @@ def search_excel(name, filepath):
     sheet = workbook.active
     # loop through name column(A)
     for cell in sheet["A"]:
-        print(cell)
+        val = cell.value
+        if "name" not in val.lower() and name.lower() == val.lower():
+            results.append(val)
 
     return results
 
 class HomeView(View):
     template_name = "manager/index.html"
+    excel_filename = "excel_files/sample.xlsx"
     context_data = {
         "page_name": "Home View"
     }
 
     def get(self, request):
+        self.context_data["file_name"] = self.excel_filename
         return render(request, template_name=self.template_name, context=self.context_data)
 
     def post(self, request):
         name = request.POST.get("name")
 
-        filepath = os.path.join(settings.BASE_DIR, "static/excel_files/sample.xlsx")
+        filepath = os.path.join(settings.BASE_DIR, "static", self.excel_filename)
         results = search_excel(name, filepath)
         self.context_data["results"] = results
+        self.context_data["file_name"] = self.excel_filename
+
         return render(request, template_name=self.template_name, context=self.context_data)
